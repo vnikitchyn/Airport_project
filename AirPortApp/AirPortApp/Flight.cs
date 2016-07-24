f4134d4c76804f6a6f6c7938efda42a4619ac94f
vnikitchyn <vnikitchyn@gmail.com>
2016-07-24 14:47:46 +0300

    ver 0.02 Flight class is created

--- AirPortApp/AirPortApp/Flight.cs
+++ AirPortApp/AirPortApp/Flight.cs
@@ -15,34 +15,65 @@ namespace AirPortApp
         public string Port { get; set; }     //city/port of arrival(departure)
         public string Airline { get; set; }  //Airline company
         public string Terminal { get; set; } // Terminal
-        enum statusEnum {checkIn, gateClosed,arrived,departedAt,unknown,canceled,expectedAt,delayed,inFlight}//"check-in","gate closed","arrived","departed at","unknown","canceled","expected at","delayed","in flight"
-        private string status;        
+        internal enum statusEnum {checkIn, gateClosed,arrived,departedAt,unknown,canceled,expectedAt,delayed,inFlight}//"check-in","gate closed","arrived","departed at","unknown","canceled","expected at","delayed","in flight"
+        public statusEnum StatusE { get; set; } // status        
+        public string Status { get; set; }
         public string Gate { get; set; } // gate
 
-        public string Status
-        {   get
-            {
-                return status;
-            }
-            set
-            {
-                statusEnum.inFlight=
-                if (statusEnum.arrived.Equals) 
-                status = value;
-            }
-        }
+     
+
         #endregion
 
-        Flight(DateTime time, int number, string port, string airline, string terminal, statusEnum status, string gate){
+        Flight(DateTime time, int number, string port, string airline, string terminal, statusEnum statusE, string gate){
             Time = time;
             Number = number;
             Port = port;
             Airline = airline;
             Terminal = terminal;
-            Status=status.ToString();
+            StatusE=statusE;
+            Status = StatusStringEnricher(statusE);
             Gate = gate;
         }
-            
+ 
+        string StatusStringEnricher(statusEnum se)
+        {
+            string res;
+             switch (se)
+            {
+                case statusEnum.arrived:
+                    res = "Arrived";
+                        break;
+                case statusEnum.canceled:
+                    res = "Cancelled";
+                    break;
+                case statusEnum.checkIn:
+                    res = "Check-in";
+                    break;
+                case statusEnum.delayed:
+                    res = "Delayed";
+                    break;
+                case statusEnum.departedAt:
+                    res = "Departed at";
+                    break;
+                case statusEnum.expectedAt:
+                    res = "Expected at";
+                    break;
+                case statusEnum.gateClosed:
+                    res = "Gate closed";
+                    break;
+                case statusEnum.inFlight:
+                    res = "In flight";
+                    break;
+                case statusEnum.unknown:
+                    res = "Unknown";
+                    break;
+                default:
+                    res = "Unknown";
+                    break;
+            }
+            return res;
+        }
+                       
 
     }
 }
